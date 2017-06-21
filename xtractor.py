df0f5953f669cc393b2cfe41dd3a7b7e30f08c1b
grimidev <calbigianmarco@gmail.com>
2017-06-21 21:28:56 +0200

    COMMIT WITH ERRORS: gtf indexing is 1 based!!!

--- xtractor.py
+++ xtractor.py
@@ -119,6 +119,12 @@ def readFastaGenome(fa_path):
     genome = {}
     genome_id = ""
 
+    """
+    genome = {
+        "$GENOME_ID" : $GENOME_STRING
+    }    
+    """
+
     # Open and read fasta file located in fa_path
     with open(fa_path) as raw_fa:
         tmp_str = ""
@@ -158,7 +164,6 @@ def grind(genome_path, gtf_path, output_dir=os.getcwd(), t_flag=False, e_flag=Fa
             elif i == "n":
                 # if choose NO then quit the program
                 sys.exit("User chose to abort the program")
-                break
             else:
                 # else prompt the question again
                 print("Wrong choice, please try again")
@@ -167,10 +172,9 @@ def grind(genome_path, gtf_path, output_dir=os.getcwd(), t_flag=False, e_flag=Fa
     gtf = readGtf(gtf_path)
     genome = readFastaGenome(genome_path)
 
-    transcript = {}
-
+    transcript_struct = {}
     """
-    transcripts = {
+    transcripts_struct= {
         $ID_GENOME : {
             $ID_GENE : {
                 $ID_TRASCRITTO : $TRASCRITTO_STRING
@@ -179,54 +183,141 @@ def grind(genome_path, gtf_path, output_dir=os.getcwd(), t_flag=False, e_flag=Fa
     }
     """
 
+    exon_struct = {}
+    """
+    exon_struct = {
+        "$START_INDEX;$END_INDEX" : {
+            "gene_id" : "$GENE_ID",
+            "transcripts" : [
+                "$TR_ID_1", "$TR_ID_2"
+            ]
+        }
+        #...
+    }
+    """
+
+    cc_struct = []
+    """
+    cc_struct = [
+        # for each exon
+        "$EXON_X1_START_INDEX;$EXON_Y1_END_INDEX",
+        "$EXON_X2_START_INDEX;$EXON_Y2_END_INDEX",
+        #...
+        "$EXON_Xn_START_INDEX;$EXON_Yn_END_INDEX"
+    ]
+    """
+
+    # loop through every genome in gtf (usually there is only 1 genome per GTF)
     for genome_id in gtf:
-        transcript[genome_id] = {}
+        transcript_struct[genome_id] = {}
+
+        # loop through every gene in the current genome
         for gene_id in gtf[genome_id]:
-            transcript[genome_id][gene_id] = {}
+            transcript_struct[genome_id][gene_id] = {}
+
+            # set unknown strand value for the current gene
+            strand = '?'
+
+            # loop through every transcript in the current gene
             for transcript_id in gtf[genome_id][gene_id]:
                 # extract only exons features from current transcript
                 exons = gtf[genome_id][gene_id][transcript_id]["exon"]
-                tmp_dict = {}
-                strand = '?'
+
+                # tmp dictionary for exons
+                tmp_exons_dict = {}
+
+                """
+                tmp_exons_dict = {
+                    "$EXON_START_INDEX" : "$EXON_NUCLEOTIDES_SEQUENCE"
+                }
+                """
+
+                # loop through every exon in the current transcript
                 for exon in exons:
+
+                    # extract start and end index of current exon
                     p0 = exon['start_index']
                     pf = exon['end_index']
 
+
                     if strand == '?':
+                        # if strand is unknown set it as the strand
+                        # of the first exon of the gene
                         strand = exon['strand']
+
                     elif strand != exon['strand']:
+                        # we have to expect that all features of the same
+                        # gene are read from the same genetic side, so if just one exon
+                        # has a different strand abort the program with error message
                         raise Exception(
                             "Fatal error: exons have different strands for the same transcript",
                             "(in genome_id: %s, gene_id: %s, transcript_id: %s" %
                             (genome_id, gene_id, transcript_id)
                         )
 
-                    if p0 not in tmp_dict:
-                        tmp_dict[p0] = genome[genome_id][p0:pf + 1]
+                    # if the p0 is not in the tmp_dict structure then the current exon
+                    # needs to be inserted into the tmp_dict structure
+                    # if p0 is already a key for tmp_dict it means that two differen
+                    # exons for the current transcript start at the same position
+                    if p0 not in tmp_exons_dict:
+                        # save exon sequence in tmp_dict
+                        tmp_exons_dict[p0] = genome[genome_id][p0:pf + 1]
                     else:
                         raise Exception(
                             "Fatal error: two different exons start at the same position",
                             "(in genome_id: %s, gene_id: %s, transcript_id: %s" %
                             (genome_id, gene_id, transcript_id)
                         )
-                tmp_dict_keys = list(tmp_dict.keys())
+
+                    # "p0;pf" key for the current exon to be put into exon_struct
+                    tmp_exon_struct_key = p0 + ";" + pf
+                    if tmp_exon_struct_key not in exon_struct:
+                        # if current exon is not in exon_struct then add it
+                        exon_struct[tmp_exon_struct_key] = {
+                            "gene_id" : gene_id,
+                            "transcripts" : [
+                                transcript_id
+                            ]
+                        }
+                    else:
+                        # else add current transcript_id to its transcripts list
+                        exon_struct[tmp_exon_struct_key]["transcripts"].append(transcript_id)
+
+                    if tmp_exon_struct_key not in cc_struct:
+                        cc_struct.append(tmp_exon_struct_key)
+
+
+
+                # extract tmp_dict keys
+                tmp_dict_keys = list(tmp_exons_dict.keys())
+
+                # sort tmp_dict_keys list
                 tmp_dict_keys.sort()
+
+                # current transcript nucleotides sequence
                 curr_transcript = ""
+
+                # read exons' sequence ordered by keys using tmp_dict_keys list
                 for key in tmp_dict_keys:
-                    curr_transcript = curr_transcript + tmp_dict[key]
+                    curr_transcript = curr_transcript + tmp_exons_dict[key]
+
+                # reverse and complement transcript if it has negative strand
                 if strand == '-':
                     curr_transcript = reverseAndComplement(curr_transcript)
 
-                transcript[genome_id][gene_id][transcript_id] = curr_transcript
+                # finally save transcript sequence in a transcript structure
+                transcript_struct[genome_id][gene_id][transcript_id] = curr_transcript
 
-    # print(json.dumps(transcript))
-
+    # whole strings (file content) to output
     transcript_output = ""
+    exome_output = ""
+    cc_output = ""
 
-    for genome_id in transcript:
-        for gene_id in transcript[genome_id]:
-            for transcript_id in transcript[genome_id][gene_id]:
-                t = transcript[genome_id][gene_id][transcript_id]
+    # format output for transcript file BEGIN
+    for genome_id in transcript_struct:
+        for gene_id in transcript_struct[genome_id]:
+            for transcript_id in transcript_struct[genome_id][gene_id]:
+                t = transcript_struct[genome_id][gene_id][transcript_id]
                 transcript_output = transcript_output + ">{0} gene_id={1} length={2}\n".format(transcript_id, gene_id, len(t))
                 start_index = 0
                 while start_index < len(t):
@@ -236,10 +327,20 @@ def grind(genome_path, gtf_path, output_dir=os.getcwd(), t_flag=False, e_flag=Fa
                         transcript_output = transcript_output + t[start_index:] + "\n"
                         break
                     start_index = start_index + FASTA_WRAP_AFTER_COLUMNS
-                transcript_output = transcript_output + "\n"
 
-    if transcript_output[-2:] == "\n\n":
+    while transcript_output[-1:] == "\n":
         transcript_output = transcript_output[0:-1]
+    # format output for transcript file END
+
+    # format output for exome file BEGIN
+    for exon_key, exon_val in exon_struct:
+        p0,pf = exon_key.split(";")
+        exome_output += ">gene_id={0} length={1} {2}\n".format(exon_val["gene_id"], str(pf-p0+1), "".join(exon_val["transcripts"]))
+        exome_output += genome[list(genome)[0]][p0-1:pf-1]
+    # format output for exome file END
+
+    # format output for cc file BEGIN
+    # format output for cc file END
 
     if not STD_OUT:
         #print to file
